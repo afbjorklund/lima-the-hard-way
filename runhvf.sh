8fd9a3a166dfd9ea7e830489f3bf4fbf0b53b0dc
Anders F Björklund <anders.f.bjorklund@gmail.com>
2024-10-30 19:59:50 +0100

    Use virtio also for qemu on macOS

--- runhvf.sh
+++ runhvf.sh
@@ -6,4 +6,4 @@ cpus=1
 memory=1024
 port=2222
 uefi=/opt/homebrew/share/qemu/edk2-aarch64-code.fd
-$qemu -accel $accel -M $machine -cpu host -smp $cpus -m $memory -hda disk.img -cdrom seed.img -net nic -net user,hostfwd=tcp::$port-:22 -bios $uefi
+$qemu -accel $accel -M $machine -cpu host -smp $cpus -m $memory -drive media=disk,index=0,if=virtio,file=disk.img -drive media=cdrom,index=2,if=virtio,file=seed.img -net nic,model=virtio -net user,hostfwd=tcp::$port-:22 -bios $uefi
