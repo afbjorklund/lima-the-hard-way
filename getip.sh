bc41b25887c64bc0dee741f50ec02c99b1607729
Anders F Björklund <anders.f.bjorklund@gmail.com>
2025-01-17 12:48:36 +0100

    Add workaround for macOS 15
    The hw_address is no longer the MAC
    So do a workaround using `arp -an`

--- getip.sh
+++ getip.sh
@@ -2,5 +2,8 @@
 # script to get IP for a MAC address
 mac=$(echo "$1" | sed -e 's/00/0/g')
 eval $(grep -C2 "hw_address=.,$mac" /var/db/dhcpd_leases)
-test -n "$lease" || exit 1
+if [ -z "$lease" ]; then
+  ip_address=$(arp -an | grep "$mac" | cut -d' ' -f2 | tr -d '()')
+  eval $(grep -B1 -A3 "ip_address=$ip_address" /var/db/dhcpd_leases)
+fi
 echo "$name has address $ip_address"
